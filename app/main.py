798165bb9819563f20c909057341a341778f1488
Meghaj Agrawal <meghajcr7@gmail.com>
2024-04-22 00:09:40 -0400

    Stage 4

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 
-def handle_connection(client_socket):
+def handle_connection(client_socket, addr):
     with client_socket:
         while True:
-            recv = client_socket.recv(1024)
-            if not recv:
+            data = client_socket.recv(1024)
+            if not data:
+                print("client at", addr, " broke connection")
                 break
-            client_socket.sendall("+PONG\r\n".encode())
+            client_socket.send("+PONG\r\n".encode())
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -16,8 +18,15 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket,addr = server_socket.accept() # wait for client
-    handle_connection(client_socket)
+    while True:
+        try:
+            (client_socket,addr) = server_socket.accept() # wait for client
+            thread = threading.Thread(target=handle_connection, args= (client_socket,addr))
+            thread.start()
+            #handle_connection(client_socket)
+        except Exception as e:
+            print("Exception occurred:", e)
+
 
 
 if __name__ == "__main__":
