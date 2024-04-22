a3865eacfb87eab3855117430391e9c0d43adfd0
Meghaj Agrawal <meghajcr7@gmail.com>
2024-04-22 01:36:19 -0400

    Stage 6

--- app/main.py
+++ app/main.py
@@ -2,6 +2,14 @@
 import socket
 import threading
 
+CLRF = "\r\n"
+PING = "ping"
+ECHO = "echo"
+SET = "set"
+GET = "get"
+
+key_val_dict = {}
+
 def handle_connection(client_socket, addr):
     with client_socket:
         while True:
@@ -18,7 +26,7 @@ def handle_connection(client_socket, addr):
                 print(e)
 
 def command_decoder(data):
-    input_request = data.split("\r\n")
+    input_request = data.split(CLRF)
     print(input_request)
     if input_request[-1] == "":
         input_request.pop()
@@ -36,6 +44,21 @@ def response_handler(input_request):
                 raise Exception("Invalid Command")
             msg = input_request[4]
             return f"${len(msg)}\r\n{input_request[4]}\r\n"
+        case "set":
+            if len(input_request) < 7:
+                raise Exception("Invalid Command")
+            key = input_request[4]
+            value = input_request[6]
+            key_val_dict[key] = value
+            return f"+OK\r\n"
+        case "get":
+            if len(input_request) < 5:
+                raise Exception("Invalid Command")
+            key = input_request[4]
+            if key not in key_val_dict:
+                return "$-1\r\n"
+            value = key_val_dict[key]
+            return f"${len(value)}\r\n{value}\r\n"
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
